259f1b386e796f4ebaabbbe46fbbb70e261127cb
Thomas Breloff <tom@breloff.com>
2015-07-06 13:30:18 -0400

    working on vectors; allow start indices; more tests

--- src/blockvector.jl
+++ src/blockvector.jl
@@ -9,30 +9,66 @@ block{T<:Number}(vec::AbstractVector{T}, startidx::Int) = Block1{T}(vec, startid
 startidx(block::Block1) = block.startidx
 endidx(block::Block1) = block.startidx + length(block.vec) - 1
 
+# --------------------------------------------------------------------------------------------------
 
 # you can create a sparse representation of blocked vectors with default val in the empty spaces
-# example: this is a nice way to create the input vector to a regression that has a bias term
+# example: this is a nice way to create the input vector to a regression that has a bias term: "v = view(length(x)+1, x)"
 type BlockVector{T} <: AbstractVector{T}
   blocks::Vector{Block1{T}}
   defaultval::T
   len::Int
 end
 
-function ArrayViews.view{T<:Number}(defaultval::T, len::Int, vs::AbstractVector{T}...)
+# function ArrayViews.view{T<:Number}(vs::AbstractVector{T}...; def::T = zero(T), len::Int = 0)
+#   blocks = Array(Block1{T}, length(vs))
+#   totlen = 0
+#   for (i,v) in enumerate(vs)
+#     blocks[i] = block(v, totlen + 1)
+#     totlen += length(v)
+#   end
+#   BlockVector{T}(blocks, def, max(totlen, len))
+# end
+
+# # ArrayViews.view{T<:Number}(len::Int, vs::AbstractVector{T}...) = view(zero(T), len, vs...)
+# # ArrayViews.view{T<:Number}(vs::AbstractVector{T}...) = view(zero(T), 0, vs...)
+
+# --------------------------------------------------------------------------------------------------
+
+function computeStartIndices{T}(vs::AbstractVector{T}...)
+  indices = Array(Int, length(vs))
+  totlen = 0
+  for (i,v) in enumerate(vs)
+    indices[i] = totlen + 1
+    totlen += length(v)
+  end
+  indices
+end
+
+# methods to create the BlockVector with a list of start indices
+
+# function ArrayViews.view{T<:Number}(indices::AbstractVector{Int}, vs::AbstractVector{T}...; def::T = zero(T), len::Int = 0)
+# function ArrayViews.view{T<:Number}(vpairs::Pair{Int,AbstractVector{T}}...; def::T = zero(T), len::Int = 0)
+function ArrayViews.view{T<:Number}(vs::AbstractVector{T}...; def::T = zero(T), len::Int = 0, indices::AbstractVector{Int} = computeStartIndices(vs...))
+  @assert length(vs) == length(indices)
   blocks = Array(Block1{T}, length(vs))
   totlen = 0
   for (i,v) in enumerate(vs)
-    blocks[i] = block(v, totlen + 1)
-    totlen += length(v)
+    startidx = indices[i]
+    @assert startidx > totlen
+    blocks[i] = block(v, startidx)
+    totlen = startidx + length(v) - 1
   end
-  BlockVector{T}(blocks, defaultval, max(totlen, len))
+  BlockVector{T}(blocks, def, max(totlen, len))
 end
 
-ArrayViews.view{T<:Number}(len::Int, vs::AbstractVector{T}...) = view(zero(T), len, vs...)
-ArrayViews.view{T<:Number}(vs::AbstractVector{T}...) = view(zero(T), 0, vs...)
+# ArrayViews.view{T<:Number}(len::Int, indices::AbstractVector{Int}, vs::AbstractVector{T}...) = view(zero(T), indices, len, vs...)
+# ArrayViews.view{T<:Number}(indices::AbstractVector{Int}, vs::AbstractVector{T}...) = view(zero(T), indices, 0, vs...)
 
+
+# --------------------------------------------------------------------------------------------------
 
 function Base.getindex(blockvec::BlockVector, i::Int)
+  checkbounds(blockvec, i)
   for block in blockvec.blocks
     sidx = startidx(block)
     i < sidx && break
@@ -41,17 +77,24 @@ function Base.getindex(blockvec::BlockVector, i::Int)
   return blockvec.defaultval
 end
 
+setindex_error() = error("You're calling BlockVector::setindex for an index without an underlying array... not currently allowed")
+
 function Base.setindex!{T<:Number}(blockvec::BlockVector{T}, data::T, i::Int)
+  checkbounds(blockvec, i)
   for block in blockvec.blocks
     sidx = startidx(block)
-    i < sidx && error("You're calling BlockVector::setindex for an index without an underlying array... not currently allowed")
+    i < sidx && setindex_error()
     if i < sidx + length(block.vec)
       block.vec[i - sidx + 1] = data
       return
     end
   end
-  return
+  setindex_error()
 end
 
 Base.length(blockvec::BlockVector) = blockvec.len
 Base.size(blockvec::BlockVector) = (length(blockvec),)
+
+# --------------------------------------------------------------------------------------------------
+
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -18,14 +18,34 @@ facts("Blox") do
   @fact_throws vw[0]
   @fact_throws vw[7]
 
+  vw[6] = 555
+  @fact vw[6] => 555
+
   # default vals at the end
-  vw = view(10, x, y)
-  for i in 1:6
+  vw = view(x, y; len=10)
+  for i in 1:5
     @fact vw[i] => i
   end
   for i in 7:10
     @fact vw[i] => 0
   end
+
+  vw = view(x, x; def=1, len=10)
+  @fact vw[3] => vw[7]
+  @fact vw[9] => 1
+  vw[1] = 33
+  @fact vw[1] => 33
+  @fact vw[1] => vw[5]
+
+  # staggered blocks
+  defval = 1
+  vw = view(x, y; def=defval, len=12, indices=[2,8])
+  @fact vw[1] => defval
+  @fact vw[2] => 33
+  @fact vw[6] => defval
+  @fact vw[8] => 5
+  @fact vw[10] => defval
+  @fact_throws vw[7] = 10
 
 end
 
