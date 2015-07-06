6641dfc74b6779f8bae3c98cd953a2000d527eb1
Thomas Breloff <tom@breloff.com>
2015-07-06 12:21:19 -0400

    first draft BlockVector

--- src/Blox.jl
+++ src/Blox.jl
@@ -1,5 +1,66 @@
 module Blox
 
+export view
+
 # package code goes here
+
+using ArrayViews
+
+immutable Block1{T}
+  vec::AbstractVector{T}
+  startidx::Int
+end
+block{T<:Number}(vec::AbstractVector{T}, startidx::Int) = Block1{T}(vec, startidx)
+
+startidx(block::Block1) = block.startidx
+endidx(block::Block1) = block.startidx + length(block.vec) - 1
+
+
+# you can create a sparse representation of blocked vectors with default val in the empty spaces
+# example: this is a nice way to create the input vector to a regression that has a bias term
+type BlockVector{T} <: AbstractVector{T}
+  blocks::Vector{Block1{T}}
+  defaultval::T
+  len::Int
+end
+
+function ArrayViews.view{T<:Number}(defaultval::T, len::Int, vs::AbstractVector{T}...)
+  blocks = Array(Block1{T}, length(vs))
+  totlen = 0
+  for (i,v) in enumerate(vs)
+    blocks[i] = block(v, totlen + 1)
+    totlen += length(v)
+  end
+  BlockVector{T}(blocks, defaultval, max(totlen, len))
+end
+
+ArrayViews.view{T<:Number}(len::Int, vs::AbstractVector{T}...) = view(zero(T), len, vs...)
+ArrayViews.view{T<:Number}(vs::AbstractVector{T}...) = view(zero(T), 0, vs...)
+
+
+function Base.getindex(blockvec::BlockVector, i::Int)
+  for block in blockvec.blocks
+    sidx = startidx(block)
+    i < sidx && break
+    i < sidx + length(block.vec) && return block.vec[i - sidx + 1]
+  end
+  return blockvec.defaultval
+end
+
+function Base.setindex!{T<:Number}(blockvec::BlockVector{T}, data::T, i::Int)
+  for block in blockvec.blocks
+    sidx = startidx(block)
+    i < sidx && error("You're calling BlockVector::setindex for an index without an underlying array... not currently allowed")
+    if i < sidx + length(block.vec)
+      block.vec[i - sidx + 1] = data
+      return
+    end
+  end
+  return
+end
+
+Base.length(blockvec::BlockVector) = blockvec.len
+Base.size(blockvec::BlockVector) = (length(blockvec),)
+
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,18 @@
-using Blox
-using Base.Test
+module BlockTests
 
-# write your own tests here
-@test 1 == 1
+using Blox
+using FactCheck
+
+@fact 1 => 1
+
+x = collect(1:4)
+y = collect(5:6)
+vw = view(x, y)
+@fact length(vw) => length(x) + length(y)
+
+for i in 1:6
+  @fact vw[i] => i
+end
+
+FactCheck.exitstatus()
+end
