cd577d26361d60083dc7493edb7ebaf32b9fb90a
Thomas Breloff <tom@breloff.com>
2015-07-06 12:32:17 -0400

    split into blockvector.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,15 +3,30 @@ module BlockTests
 using Blox
 using FactCheck
 
-@fact 1 => 1
+facts("Blox") do
+  @fact 1 => 1
 
-x = collect(1:4)
-y = collect(5:6)
-vw = view(x, y)
-@fact length(vw) => length(x) + length(y)
+  x = collect(1:4)
+  y = collect(5:6)
+  vw = view(x, y)
+  @fact length(vw) => length(x) + length(y)
+  @fact size(vw) => (length(vw),)
 
-for i in 1:6
-  @fact vw[i] => i
+  for i in 1:6
+    @fact vw[i] => i
+  end
+  @fact_throws vw[0]
+  @fact_throws vw[7]
+
+  # default vals at the end
+  vw = view(10, x, y)
+  for i in 1:6
+    @fact vw[i] => i
+  end
+  for i in 7:10
+    @fact vw[i] => 0
+  end
+
 end
 
 FactCheck.exitstatus()
